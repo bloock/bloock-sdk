ce3cf81ec2c9330549855bbf9365cf3ba307c93d
Marc Baqué <marcbaque96@gmail.com>
2022-07-07 12:31:14 +0200

    Added http client implementation for reqwest (#2)
    Co-authored-by: Marc Baqué <marc@bloock.com>

--- src/infrastructure/mod.rs
+++ src/infrastructure/mod.rs
@@ -1,2 +1,16 @@
+use thiserror::Error as ThisError;
+
 pub mod hashing;
 pub mod http;
+
+pub type Result<T> = std::result::Result<T, InfrastructureError>;
+
+#[derive(ThisError, Debug)]
+pub enum InfrastructureError {
+    #[error("API connected by HttpClient found an error: {0}")]
+    HttpClientApiError(String),
+    #[error("Serialize error - {0}")]
+    SerializeError(String),
+    #[error("Reqwest error - {0}")]
+    ReqwestError(#[from] reqwest::Error),
+}
