3f327f54e91c70d824d678c2eb267ac3fdb1d381
Marc Baqué <marc@bloock.com>
2022-08-02 11:15:27 +0200

    Revert "add java project"
    This reverts commit 66f89789d2e95959086262d7ae028b0f1199da42.

---
+++ bloock-bridge/src/error.rs
@@ -0,0 +1,7 @@
+use thiserror::Error;
+
+#[derive(Error, Debug, PartialEq, Eq, Clone)]
+pub enum BridgeError {
+    #[error("Couldn't serialize response: {0}")]
+    ResponseSerialization(String),
+}
