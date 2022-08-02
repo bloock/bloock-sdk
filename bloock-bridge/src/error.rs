66f89789d2e95959086262d7ae028b0f1199da42
Neifi <neifialcantara@gmail.com>
2022-08-02 10:30:13 +0200

    add java project

--- bloock-bridge/src/error.rs
+++
@@ -1,7 +0,0 @@
-use thiserror::Error;
-
-#[derive(Error, Debug, PartialEq, Eq, Clone)]
-pub enum BridgeError {
-    #[error("Couldn't serialize response: {0}")]
-    ResponseSerialization(String),
-}
