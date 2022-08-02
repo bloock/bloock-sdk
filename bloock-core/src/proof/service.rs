cf776460b0c1ab8ba911f62ea273de81cbffb84e
josepmdc <jdomingocatafal@gmail.com>
2022-08-02 10:06:20 +0200

    Finish tests for verify proof

--- bloock-core/src/proof/service.rs
+++ bloock-core/src/proof/service.rs
@@ -325,13 +325,75 @@ mod tests {
         assert_eq!(result.get_hash(), root);
     }
 
-    // #[tokio::test]
-    // async fn test_verify_proof_keccak_2() {
-    //     unimplemented!();
-    // }
+    #[tokio::test]
+    async fn test_verify_proof_keccak_2() {
+        let leaves = vec![
+            "02aae7e86eb50f61a62083a320475d9d60cbd52749dbf08fa942b1b97f50aee5".to_string(),
+            "02aae7e86eb50f61a62083a320475d9d60cbd52749dbf08fa942b1b97f50aee5".to_string(),
+            "5e1712aca5f3925fc0ce628e7da2e1e407e2cc7b358e83a7152b1958f7982dab".to_string(),
+        ];
 
-    // #[tokio::test]
-    // async fn test_verify_proof_keccak_3() {
-    //     unimplemented!();
-    // }
+        let nodes = vec![
+            "1ca0e9d9a206f08d38a4e2cf485351674ffc9b0f3175e0cb6dbd8e0e19829b97".to_string(),
+            "1ca0e9d9a206f08d38a4e2cf485351674ffc9b0f3175e0cb6dbd8e0e19829b97".to_string(),
+            "1509877db1aa81c699a144d1a240c5d463c9ff08b2df489b40a35802844baeb6".to_string(),
+            "54944fcea707a57048c17ca7453fa5078a031143b44629776750e7f0ff7940f0".to_string(),
+            "d6f9bcd042be70b39b65dc2a8168858606b0a2fcf6d02c0a1812b1804efc0c37".to_string(),
+            "e663ec001b81b96eceabd1b766d49ec5d99adedc3e5f03d245b0d90f603f66d3".to_string(),
+        ];
+
+        let depth = "000500050004000400040004000400030001".to_string();
+        let bitmap = "6d80".to_string();
+        let root = "7e1f3c7e6d3515389b6117cc8c1ef5512d51c59743dc097c70de405a91861d2b".to_string();
+        let anchor = Anchor {
+            id: 1,
+            block_roots: vec![],
+            networks: vec![],
+            root: "".to_string(),
+            status: "pending".to_string(),
+        };
+
+        let mut http = MockClient::default();
+        let service = configure_test(Arc::new(http));
+        let result = service
+            .verify_proof(Proof {
+                leaves,
+                nodes,
+                depth,
+                bitmap,
+                anchor,
+            })
+            .unwrap();
+
+        assert_eq!(result.get_hash(), root);
+    }
+
+    #[tokio::test]
+    async fn test_verify_proof_keccak_3() {
+        let leaves = vec!["0000000000000000000000000000000000000000000000000000000000000000".to_string()];
+        let nodes = vec!["f49d70da1c2c8989766908e06b8d2277a6954ec8533696b9a404b631b0b7735a".to_string()];
+        let depth = "00010001".to_string();
+        let bitmap = "4000".to_string();
+        let root = "5c67902dc31624d9278c286ef4ce469451d8f1d04c1edb29a5941ca0e03ddc8d".to_string();
+        let anchor = Anchor {
+            id: 1,
+            block_roots: vec![],
+            networks: vec![],
+            root: "".to_string(),
+            status: "pending".to_string(),
+        };
+        let mut http = MockClient::default();
+        let service = configure_test(Arc::new(http));
+        let result = service
+            .verify_proof(Proof {
+                leaves,
+                nodes,
+                depth,
+                bitmap,
+                anchor,
+            })
+            .unwrap();
+
+        assert_eq!(result.get_hash(), root);
+    }
 }
