1871fc74bfc1dc278f8437e1b753c608ea00c1dd
josepmdc <jdomingocatafal@gmail.com>
2022-08-11 11:28:21 +0200

    Add record to go sdk

--- bloock-bridge/go/internal/bridge/bridge.go
+++ bloock-bridge/go/internal/bridge/bridge.go
@@ -5,13 +5,15 @@ import "github.com/bloock/go-bridge/internal/bridge/proto"
 type BloockBridge struct {
 	greeting proto.GreeterClient
 	anchor   proto.AnchorServiceClient
+	record   proto.RecordServiceClient
 }
 
 func NewBloockBridge() BloockBridge {
 	conn := NewBloockConnection()
 	return BloockBridge{
 		greeting: proto.NewGreeterClient(conn),
-		anchor: proto.NewAnchorServiceClient(conn),
+		anchor:   proto.NewAnchorServiceClient(conn),
+        record: proto.NewRecordServiceClient(conn),
 	}
 }
 
@@ -20,5 +22,9 @@ func (b *BloockBridge) Greeting() proto.GreeterClient {
 }
 
 func (b *BloockBridge) Anchor() proto.AnchorServiceClient {
-    return b.anchor
+	return b.anchor
+}
+
+func (b *BloockBridge) Record() proto.RecordServiceClient {
+	return b.record
 }
