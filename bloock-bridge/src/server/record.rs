76142595cacc58690112391272530a9b37654f60
edutomesco <eduard.escoruela@gmail.com>
2022-09-20 13:32:17 +0200

    master | add builders to bridge

--- bloock-bridge/src/server/record.rs
+++ bloock-bridge/src/server/record.rs
@@ -1,12 +1,21 @@
+use std::convert::TryInto;
+
 use async_trait::async_trait;
-use bloock_core::{client, error::BloockError, record::entity::record::Record as RecordCore};
+use bloock_core::{
+    client,
+    error::BloockError,
+    record::builder::{Builder, RecordBuilder},
+    record::entity::record::Record as RecordCore,
+    AesEncrypter, AesEncrypterArgs, EcsdaSigner, EcsdaSignerArgs,
+};
 
 use super::response_types::ResponseType;
 use crate::{
     entity_mappings::config::map_config,
     error::{config_data_error, BridgeError},
     items::{
-        Error, Record, RecordBuilderResponse, RecordHash, RecordServiceHandler, SendRecordsResponse,
+        Encrypter, EncrypterAlg, Error, Record, RecordBuilderResponse, RecordHash,
+        RecordServiceHandler, SendRecordsResponse, Signer, SignerAlg,
     },
 };
 
@@ -21,12 +30,6 @@ impl From<Record> for ResponseType {
         ResponseType::Record(res)
     }
 }
-
-/*impl From<FromHexResponse> for ResponseType {
-    fn from(res: FromHexResponse) -> Self {
-        ResponseType::RecordFromHex(res)
-    }
-}*/
 
 pub struct RecordServer {}
 
@@ -85,22 +88,160 @@ impl RecordServiceHandler for RecordServer {
     }
 
     async fn get_hash(&self, _req: Record) -> RecordHash {
+        let record: RecordCore = match _req.try_into() {
+            Ok(record) => record,
+            Err(e) => {
+                return RecordHash {
+                    hash: "".to_string(),
+                    error: Some(Error {
+                        kind: BridgeError::RecordError.to_string(),
+                        message: e.to_string(),
+                    }),
+                }
+            }
+        };
+        let hash = record.get_hash();
         RecordHash {
-            hash: "".to_string(),
+            hash: hash,
             error: None,
         }
     }
 
-    async fn build_record(
+    async fn build_record_from_string(
         &self,
-        _req: crate::items::RecordBuilderRequest,
+        req: crate::items::RecordBuilderFromStringRequest,
     ) -> RecordBuilderResponse {
-        RecordBuilderResponse {
-            record: None,
-            error: Some(Error {
-                kind: BridgeError::RecordError.to_string(),
-                message: "not implemented".to_string(),
-            }),
+        let builder = RecordBuilder::from_string(req.payload);
+        build_record(builder, req.signer, req.encrypter)
+    }
+
+    async fn build_record_from_hex(
+        &self,
+        req: crate::items::RecordBuilderFromStringRequest,
+    ) -> RecordBuilderResponse {
+        let builder = RecordBuilder::from_hex(req.payload).unwrap();
+        build_record(builder, req.signer, req.encrypter)
+    }
+
+    async fn build_record_from_json(
+        &self,
+        req: crate::items::RecordBuilderFromStringRequest,
+    ) -> RecordBuilderResponse {
+        let builder = RecordBuilder::from_json(req.payload).unwrap();
+        build_record(builder, req.signer, req.encrypter)
+    }
+
+    async fn build_record_from_file(
+        &self,
+        req: crate::items::RecordBuilderFromBytesRequest,
+    ) -> RecordBuilderResponse {
+        let builder = RecordBuilder::from_file(req.payload);
+        build_record(builder, req.signer, req.encrypter)
+    }
+
+    async fn build_record_from_bytes(
+        &self,
+        req: crate::items::RecordBuilderFromBytesRequest,
+    ) -> RecordBuilderResponse {
+        let builder = RecordBuilder::from_file(req.payload);
+        build_record(builder, req.signer, req.encrypter)
+    }
+}
+
+fn build_record(
+    mut builder: Builder,
+    signer: Option<Signer>,
+    encrypter: Option<Encrypter>,
+) -> RecordBuilderResponse {
+    if let Some(signer) = signer {
+        let signer = match SignerAlg::from_i32(signer.alg) {
+            Some(SignerAlg::Es256k) => {
+                let signer_arguments = match signer.args {
+                    Some(signer_arguments) => signer_arguments,
+                    None => {
+                        return RecordBuilderResponse {
+                            record: None,
+                            error: Some(Error {
+                                kind: BridgeError::RecordError.to_string(),
+                                message: "no private key provided".to_string(),
+                            }),
+                        }
+                    }
+                };
+                EcsdaSigner::new(EcsdaSignerArgs::new(&signer_arguments.private_key))
+            }
+            None => {
+                return RecordBuilderResponse {
+                    record: None,
+                    error: Some(Error {
+                        kind: BridgeError::RecordError.to_string(),
+                        message: "invalid signer provided".to_string(),
+                    }),
+                }
+            }
+        };
+        builder = builder.with_signer(signer);
+    }
+
+    if let Some(encrypt) = encrypter {
+        let encrypter = match EncrypterAlg::from_i32(encrypt.alg) {
+            Some(EncrypterAlg::A256gcm) => {
+                let encrypter_arguments = match encrypt.args {
+                    Some(encrypter_arguments) => encrypter_arguments,
+                    None => {
+                        return RecordBuilderResponse {
+                            record: None,
+                            error: Some(Error {
+                                kind: BridgeError::RecordError.to_string(),
+                                message: "no secret provided".to_string(),
+                            }),
+                        }
+                    }
+                };
+                AesEncrypter::new(AesEncrypterArgs::new(&encrypter_arguments.secret))
+            }
+            None => {
+                return RecordBuilderResponse {
+                    record: None,
+                    error: Some(Error {
+                        kind: BridgeError::RecordError.to_string(),
+                        message: "invalid encrypter provided".to_string(),
+                    }),
+                }
+            }
+        };
+        builder = builder.with_encrypter(encrypter);
+    }
+
+    let record: Record = match builder.build() {
+        Ok(record) => match record.try_into() {
+            Ok(record) => record,
+            Err(e) => {
+                return RecordBuilderResponse {
+                    record: None,
+                    error: Some(Error {
+                        kind: BridgeError::RecordError.to_string(),
+                        message: e.to_string(),
+                    }),
+                }
+            }
+        },
+        Err(e) => {
+            return RecordBuilderResponse {
+                record: None,
+                error: Some(Error {
+                    kind: BridgeError::RecordError.to_string(),
+                    message: e.to_string(),
+                }),
+            }
         }
+    };
+
+    RecordBuilderResponse {
+        record: Some(record),
+        error: Some(Error {
+            kind: BridgeError::RecordError.to_string(),
+            message: "not implemented".to_string(),
+        }),
     }
 }
