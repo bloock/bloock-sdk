6491988f1a1b4bd2870aa7540505ca01e7f6b433
marcbaque <marc@bloock.com>
2022-09-22 13:03:24 +0200

    fix golang ffi directories

--- languages/go/internal/ffi/ffi.go
+++ languages/go/internal/ffi/ffi.go
@@ -4,10 +4,8 @@ package ffi
 // #include <stdint.h>
 // #include <stdlib.h>
 // #include "native/bloock_bridge.h"
-// #cgo linux,386 LDFLAGS: ${SRCDIR}/native/i686-unknown-linux-gnu/libbloock_bridge.a -ldl -lm
 // #cgo linux,amd64 LDFLAGS: ${SRCDIR}/native/x86_64-unknown-linux-gnu/libbloock_bridge.a -ldl -lm
 // #cgo linux,arm64 LDFLAGS: ${SRCDIR}/native/aarch64-unknown-linux-gnu/libbloock_bridge.a -ldl -lm
-// #cgo windows,386 LDFLAGS: ${SRCDIR}/native/i686-pc-windows-gnu/libbloock_bridge.a -lm
 // #cgo windows,amd64 LDFLAGS: ${SRCDIR}/native/x86_64-pc-windows-gnu/libbloock_bridge.a -lm
 // #cgo darwin,amd64 LDFLAGS: ${SRCDIR}/native/x86_64-apple-darwin/libbloock_bridge.a -ldl -lm -framework Security
 // #cgo darwin,arm64 LDFLAGS: ${SRCDIR}/native/aarch64-apple-darwin/libbloock_bridge.a -ldl -lm -framework Security
