d9a43abdac2550cebedbb081f1dfe04a341cac65
josepmdc <jdomingocatafal@gmail.com>
2022-09-27 12:03:00 +0200

    Add signer.ts

---
+++ languages/js/src/entity/signer.ts
@@ -0,0 +1,31 @@
+import * as proto from '../bridge/proto/record';
+
+export interface Signer {
+  alg: proto.SignerAlg;
+  args: SignerArgs;
+}
+
+export class EcsdaSigner implements Signer {
+  alg: proto.SignerAlg;
+  args: SignerArgs;
+
+  constructor(privateKey: string) {
+    this.alg = proto.SignerAlg.ES256K;
+    this.args = new SignerArgs(privateKey);
+  }
+
+  public toProto(): proto.Signer {
+    return proto.Signer.fromPartial({alg: this.alg, args: this.args.toProto()});
+  }
+}
+
+export class SignerArgs {
+  privateKey: string;
+  constructor(privateKey: string) {
+    this.privateKey = privateKey;
+  }
+
+  public toProto(): proto.SignerArgs {
+    return proto.SignerArgs.fromPartial({privateKey: this.privateKey});
+  }
+}
