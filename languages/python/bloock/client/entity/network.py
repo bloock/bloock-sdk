73ced4f68242f579fba60e614164c9dcbd020f01
josepmdc <jdomingocatafal@gmail.com>
2022-10-17 12:36:39 +0200

    [Python] Fix Network enum was starting from 1

--- languages/python/bloock/client/entity/network.py
+++ languages/python/bloock/client/entity/network.py
@@ -3,11 +3,11 @@ from bloock._bridge.proto.config_pb2 import Network as NetworkProto
 
 
 class Network(Enum):
-    ETHEREUM_MAINNET = 1
-    ETHEREUM_RINKEBY = 2
-    ETHEREUM_GOERLI = 3
-    GNOSIS_CHAIN = 4
-    BLOOCK_CHAIN = 5
+    ETHEREUM_MAINNET = 0
+    ETHEREUM_RINKEBY = 1
+    ETHEREUM_GOERLI = 2
+    GNOSIS_CHAIN = 3
+    BLOOCK_CHAIN = 4
 
     @classmethod
     def from_proto(cls, network: NetworkProto):
