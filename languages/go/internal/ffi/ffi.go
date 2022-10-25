f2b67085cdc49ad6ce90711a52dc7a90e1cfc514
marcbaque <marc@bloock.com>
2022-10-25 16:30:41 +0200

    Add -lpthread to ldflags

--- languages/go/internal/ffi/ffi.go
+++ languages/go/internal/ffi/ffi.go
@@ -4,7 +4,7 @@ package ffi
 // #include <stdint.h>
 // #include <stdlib.h>
 // #include "native/bloock_bridge.h"
-// #cgo linux,amd64 LDFLAGS: ${SRCDIR}/native/x86_64_unknown_linux_musl/libbloock_bridge.a -ldl -lm
+// #cgo linux,amd64 LDFLAGS: ${SRCDIR}/native/x86_64_unknown_linux_musl/libbloock_bridge.a -ldl -lm -lpthread
 // #cgo windows,amd64 LDFLAGS: ${SRCDIR}/native/x86_64_pc_windows_gnu/libbloock_bridge_windows.a -lm -lws2_32 -luserenv -lbcrypt
 // #cgo darwin,amd64 LDFLAGS: ${SRCDIR}/native/x86_64_apple_darwin/libbloock_bridge.a -ldl -lm -framework Security
 // #cgo darwin,arm64 LDFLAGS: ${SRCDIR}/native/aarch64_apple_darwin/libbloock_bridge.a -ldl -lm -framework Security
