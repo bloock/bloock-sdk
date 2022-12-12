e6af5d44bbd09a3f1c8018ffd8464b723f2e9566
josepmdc <jdomingocatafal@gmail.com>
2022-12-12 13:05:28 +0100

    Java fmt

--- languages/java/src/main/java/com/bloock/sdk/entity/EciesKeyPair.java
+++ languages/java/src/main/java/com/bloock/sdk/entity/EciesKeyPair.java
@@ -3,11 +3,11 @@ package com.bloock.sdk.entity;
 import com.bloock.sdk.bridge.proto.RecordOuterClass.GenerateEciesKeyPairResponse;
 
 public class EciesKeyPair extends KeyPair {
-    EciesKeyPair(String publicKey, String privateKey) {
-        super(publicKey, privateKey);
-    }
+  EciesKeyPair(String publicKey, String privateKey) {
+    super(publicKey, privateKey);
+  }
 
-    public static EciesKeyPair fromProto(GenerateEciesKeyPairResponse keys) {
-        return new EciesKeyPair(keys.getPublicKey(), keys.getPrivateKey());
-    }
+  public static EciesKeyPair fromProto(GenerateEciesKeyPairResponse keys) {
+    return new EciesKeyPair(keys.getPublicKey(), keys.getPrivateKey());
+  }
 }
