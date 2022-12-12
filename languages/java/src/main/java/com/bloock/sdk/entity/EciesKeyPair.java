6a85dd206a973fc27ac961f2d4c143ec474de4f3
josepmdc <jdomingocatafal@gmail.com>
2022-12-12 12:58:22 +0100

    Add missing java files

---
+++ languages/java/src/main/java/com/bloock/sdk/entity/EciesKeyPair.java
@@ -0,0 +1,13 @@
+package com.bloock.sdk.entity;
+
+import com.bloock.sdk.bridge.proto.RecordOuterClass.GenerateEciesKeyPairResponse;
+
+public class EciesKeyPair extends KeyPair {
+    EciesKeyPair(String publicKey, String privateKey) {
+        super(publicKey, privateKey);
+    }
+
+    public static EciesKeyPair fromProto(GenerateEciesKeyPairResponse keys) {
+        return new EciesKeyPair(keys.getPublicKey(), keys.getPrivateKey());
+    }
+}
