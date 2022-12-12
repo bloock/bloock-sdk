82a5f4b57c54a2e78989cd18cfd7339079993188
Josep M. Domingo Catafal <jdomingocatafal@gmail.com>
2022-12-12 09:44:43 +0100

    EIT-2785 | Test encryption interoperability with data availability services (#48)
    * Start implementing RSA
    
    * RSA working
    
    * Implement RSA on Go interface
    
    * Implement RSA python interface
    
    * Implement RSA on JS interface
    
    * fmt
    
    * Fix clippy
    
    * Implement RSA on Java interface
    
    * Fix js
    
    * Fix Java
    
    * fmt java
    
    * Replace expect with map_err
    
    * Clean up Go E2E test and add encryption data availability tests
    
    * Clean up JS E2E test and add encryption data availability tests
    
    * Clean up Python E2E test and add encryption data availability tests
    
    * Clean up Java E2E test and add encryption data availability tests
    
    * Fmt

--- languages/go/internal/test/end_to_end_test.go
+++ languages/go/internal/test/end_to_end_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/bloock/bloock-sdk-go/v2/builder"
+	"github.com/bloock/bloock-sdk-go/v2/client"
 	"github.com/bloock/bloock-sdk-go/v2/client/entity"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -15,119 +16,20 @@ func TestEndToEnd(t *testing.T) {
 	t.Run("E2E using all the builders", func(t *testing.T) {
 		records := []string{}
 
-		record, err := builder.NewRecordBuilderFromString("Hello world").Build()
-		require.NoError(t, err)
-		hash, err := record.GetHash()
-		require.NoError(t, err)
-		assert.Equal(t, "ed6c11b0b5b808960df26f5bfc471d04c1995b0ffd2055925ad1be28d6baadfd", hash)
+		records = append(records, testFromString(t))
+		records = append(records, testFromBytes(t))
+		records = append(records, testFromHex(t))
+		records = append(records, testFromJson(t))
+		records = append(records, testFromFile(t))
+		records = append(records, testEcsdaSignature(t, sdk))
 
-		result, err := record.Publish(entity.NewHostedPublisher())
-		require.NoError(t, err)
-		assert.Equal(t, hash, result)
+		testFromLoader(t)
 
-		record, err = builder.NewRecordBuilderFromLoader(entity.NewHostedLoader(result)).Build()
-		require.NoError(t, err)
-		hash, err = record.GetHash()
-		require.NoError(t, err)
-		assert.Equal(t, result, hash)
+		testAesEncryption(t)
+		testAesEncryptionDataAvailability(t)
 
-		records = append(records, hash)
-
-		record, err = builder.NewRecordBuilderFromBytes([]byte{1, 2, 3, 4, 5}).Build()
-		require.NoError(t, err)
-		hash, err = record.GetHash()
-		require.NoError(t, err)
-		assert.Equal(t, "7d87c5ea75f7378bb701e404c50639161af3eff66293e9f375b5f17eb50476f4", hash)
-		records = append(records, hash)
-
-		record, err = builder.NewRecordBuilderFromHex("1234567890abcdef").Build()
-		require.NoError(t, err)
-		hash, err = record.GetHash()
-		require.NoError(t, err)
-		assert.Equal(t, "ed8ab4fde4c4e2749641d9d89de3d920f9845e086abd71e6921319f41f0e784f", hash)
-		records = append(records, hash)
-
-		record, err = builder.NewRecordBuilderFromJSON("{\"hello\":\"world\"}").Build()
-		require.NoError(t, err)
-		hash, err = record.GetHash()
-		require.NoError(t, err)
-		assert.Equal(t, "586e9b1e1681ba3ebad5ff5e6f673d3e3aa129fcdb76f92083dbc386cdde4312", hash)
-		records = append(records, hash)
-
-		payload := "Hello world 2"
-		encryptedRecord, err := builder.NewRecordBuilderFromString(payload).
-			WithEncrypter(entity.NewAesEncrypter("some_password")).
-			Build()
-		require.NoError(t, err)
-		assert.NotEqual(t, payload, string(encryptedRecord.Retrieve()))
-
-		record, err = builder.NewRecordBuilderFromRecord(encryptedRecord).
-			WithDecrypter(entity.NewAesDecrypter("some_password")).
-			Build()
-		require.NoError(t, err)
-		assert.Equal(t, payload, string(record.Retrieve()))
-
-		hash, err = record.GetHash()
-		require.NoError(t, err)
-		assert.Equal(t, "96d59e2ea7cec4915c415431e6adb115e3c0c728928773bcc8e7d143b88bfda6", hash)
-
-		_, err = builder.NewRecordBuilderFromRecord(encryptedRecord).
-			WithDecrypter(entity.NewAesDecrypter("incorrect_password")).
-			Build()
-		require.Error(t, err)
-
-		keypair, err := sdk.GenerateRsaKeyPair()
-
-		encryptedRecord, err = builder.NewRecordBuilderFromString(payload).
-			WithEncrypter(entity.NewRsaEncrypter(keypair.PublicKey)).
-			Build()
-		require.NoError(t, err)
-		assert.NotEqual(t, payload, string(encryptedRecord.Payload))
-
-		record, err = builder.NewRecordBuilderFromRecord(encryptedRecord).
-			WithDecrypter(entity.NewRsaDecrypter(keypair.PrivateKey)).
-			Build()
-		require.NoError(t, err)
-		assert.Equal(t, payload, string(record.Retrieve()))
-
-		hash, err = record.GetHash()
-		require.NoError(t, err)
-		assert.Equal(t, "96d59e2ea7cec4915c415431e6adb115e3c0c728928773bcc8e7d143b88bfda6", hash)
-		records = append(records, hash)
-
-		record, err = builder.NewRecordBuilderFromFile([]byte{2, 3, 4, 5, 6}).Build()
-		require.NoError(t, err)
-		hash, err = record.GetHash()
-		require.NoError(t, err)
-		assert.Equal(t, "507aa5dd7b2e52180b764db13c8289ed204109cafe2ef4e453366da8654dc446", hash)
-		records = append(records, hash)
-
-		keys, err := sdk.GenerateKeys()
-		require.NoError(t, err)
-
-		record, err = builder.
-			NewRecordBuilderFromString("Hello world 3").
-			WithSigner(entity.NewEcsdaSigner(keys.PrivateKey)).
-			Build()
-		require.NoError(t, err)
-
-		keys, err = sdk.GenerateKeys()
-		require.NoError(t, err)
-
-		recordWithMultipleSignatures, err := builder.
-			NewRecordBuilderFromRecord(record).
-			WithSigner(entity.NewEcsdaSigner(keys.PrivateKey)).
-			Build()
-
-		require.NoError(t, err)
-		hash, err = recordWithMultipleSignatures.GetHash()
-		require.NoError(t, err)
-		assert.Equal(t, hash, "79addac952bf2c80b87161407ac455cf389b17b98e8f3e75ed9638ab06481f4f")
-		records = append(records, hash)
-
-		signatures, err := recordWithMultipleSignatures.GetSignatures()
-		require.NoError(t, err)
-		assert.Equal(t, len(signatures), 2)
+		testRsaEncryption(t, sdk)
+		testRsaEncryptionDataAvailability(t, sdk)
 
 		receipt, err := sdk.SendRecords(records)
 		require.NoError(t, err)
@@ -159,4 +61,224 @@ func TestEndToEnd(t *testing.T) {
 
 		assert.Equal(t, timestampValidateRoot, timestampVerifyRecords)
 	})
+}
+
+func testFromString(t *testing.T) string {
+	record, err := builder.NewRecordBuilderFromString("Hello world").Build()
+	require.NoError(t, err)
+
+	hash, err := record.GetHash()
+	require.NoError(t, err)
+
+	assert.Equal(t, "ed6c11b0b5b808960df26f5bfc471d04c1995b0ffd2055925ad1be28d6baadfd", hash)
+
+	return hash
+}
+
+func testFromLoader(t *testing.T) {
+	record, err := builder.NewRecordBuilderFromString("Hello world").Build()
+	require.NoError(t, err)
+
+	hash, err := record.GetHash()
+	require.NoError(t, err)
+
+	result, err := record.Publish(entity.NewHostedPublisher())
+	require.NoError(t, err)
+	assert.Equal(t, hash, result)
+
+	record, err = builder.NewRecordBuilderFromLoader(entity.NewHostedLoader(result)).Build()
+	require.NoError(t, err)
+
+	hash, err = record.GetHash()
+	require.NoError(t, err)
+
+	assert.Equal(t, result, hash)
+}
+
+func testFromBytes(t *testing.T) string {
+	record, err := builder.NewRecordBuilderFromBytes([]byte{1, 2, 3, 4, 5}).Build()
+	require.NoError(t, err)
+
+	hash, err := record.GetHash()
+	require.NoError(t, err)
+
+	assert.Equal(t, "7d87c5ea75f7378bb701e404c50639161af3eff66293e9f375b5f17eb50476f4", hash)
+
+	return hash
+}
+
+func testFromHex(t *testing.T) string {
+	record, err := builder.NewRecordBuilderFromHex("1234567890abcdef").Build()
+	require.NoError(t, err)
+
+	hash, err := record.GetHash()
+	require.NoError(t, err)
+
+	assert.Equal(t, "ed8ab4fde4c4e2749641d9d89de3d920f9845e086abd71e6921319f41f0e784f", hash)
+
+	return hash
+}
+
+func testFromJson(t *testing.T) string {
+	record, err := builder.NewRecordBuilderFromJSON("{\"hello\":\"world\"}").Build()
+	require.NoError(t, err)
+
+	hash, err := record.GetHash()
+	require.NoError(t, err)
+
+	assert.Equal(t, "586e9b1e1681ba3ebad5ff5e6f673d3e3aa129fcdb76f92083dbc386cdde4312", hash)
+
+	return hash
+}
+
+func testFromFile(t *testing.T) string {
+	record, err := builder.NewRecordBuilderFromFile([]byte{2, 3, 4, 5, 6}).Build()
+	require.NoError(t, err)
+
+	hash, err := record.GetHash()
+	require.NoError(t, err)
+
+	assert.Equal(t, "507aa5dd7b2e52180b764db13c8289ed204109cafe2ef4e453366da8654dc446", hash)
+
+	return hash
+}
+
+func testAesEncryption(t *testing.T) {
+	payload := "Hello world 2"
+	password := "some_password"
+	encryptedRecord, err := builder.NewRecordBuilderFromString(payload).
+		WithEncrypter(entity.NewAesEncrypter(password)).
+		Build()
+
+	require.NoError(t, err)
+	assert.NotEqual(t, payload, string(encryptedRecord.Retrieve()))
+
+	_, err = builder.NewRecordBuilderFromRecord(encryptedRecord).
+		WithDecrypter(entity.NewAesDecrypter("incorrect_password")).
+		Build()
+	require.Error(t, err)
+
+	decryptedRecord, err := builder.NewRecordBuilderFromRecord(encryptedRecord).
+		WithDecrypter(entity.NewAesDecrypter(password)).
+		Build()
+
+	require.NoError(t, err)
+	assert.Equal(t, payload, string(decryptedRecord.Retrieve()))
+
+	hash, err := decryptedRecord.GetHash()
+	require.NoError(t, err)
+	assert.Equal(t, "96d59e2ea7cec4915c415431e6adb115e3c0c728928773bcc8e7d143b88bfda6", hash)
+}
+
+func testAesEncryptionDataAvailability(t *testing.T) {
+	payload := "Hello world 2"
+	password := "some_password"
+
+	encryptedRecord, err := builder.NewRecordBuilderFromString(payload).
+		WithEncrypter(entity.NewAesEncrypter(password)).
+		Build()
+
+	assert.NotEqual(t, payload, string(encryptedRecord.Retrieve()))
+
+	result, err := encryptedRecord.Publish(entity.NewHostedPublisher())
+	require.NoError(t, err)
+
+	loadedRecord, err := builder.NewRecordBuilderFromLoader(entity.NewHostedLoader(result)).Build()
+	require.NoError(t, err)
+
+	assert.Equal(t, encryptedRecord.Retrieve(), loadedRecord.Retrieve())
+
+	decryptedRecord, err := builder.NewRecordBuilderFromRecord(loadedRecord).
+		WithDecrypter(entity.NewAesDecrypter(password)).
+		Build()
+
+	require.NoError(t, err)
+	assert.Equal(t, payload, string(decryptedRecord.Retrieve()))
+
+	hash, err := decryptedRecord.GetHash()
+	require.NoError(t, err)
+	assert.Equal(t, "96d59e2ea7cec4915c415431e6adb115e3c0c728928773bcc8e7d143b88bfda6", hash)
+}
+
+func testRsaEncryption(t *testing.T, sdk client.Client) {
+	payload := "Hello world 2"
+	keypair, err := sdk.GenerateRsaKeyPair()
+
+	encryptedRecord, err := builder.NewRecordBuilderFromString(payload).
+		WithEncrypter(entity.NewRsaEncrypter(keypair.PublicKey)).
+		Build()
+
+	require.NoError(t, err)
+	assert.NotEqual(t, payload, string(encryptedRecord.Payload))
+
+	record, err := builder.NewRecordBuilderFromRecord(encryptedRecord).
+		WithDecrypter(entity.NewRsaDecrypter(keypair.PrivateKey)).
+		Build()
+
+	require.NoError(t, err)
+	assert.Equal(t, payload, string(record.Retrieve()))
+
+	hash, err := record.GetHash()
+	require.NoError(t, err)
+	assert.Equal(t, "96d59e2ea7cec4915c415431e6adb115e3c0c728928773bcc8e7d143b88bfda6", hash)
+}
+
+func testRsaEncryptionDataAvailability(t *testing.T, sdk client.Client) {
+	payload := "Hello world 2"
+	keypair, err := sdk.GenerateRsaKeyPair()
+
+	encryptedRecord, err := builder.NewRecordBuilderFromString(payload).
+		WithEncrypter(entity.NewRsaEncrypter(keypair.PublicKey)).
+		Build()
+
+	assert.NotEqual(t, payload, string(encryptedRecord.Retrieve()))
+
+	result, err := encryptedRecord.Publish(entity.NewHostedPublisher())
+	require.NoError(t, err)
+
+	loadedRecord, err := builder.NewRecordBuilderFromLoader(entity.NewHostedLoader(result)).Build()
+	require.NoError(t, err)
+
+	assert.Equal(t, encryptedRecord.Retrieve(), loadedRecord.Retrieve())
+
+	decryptedRecord, err := builder.NewRecordBuilderFromRecord(loadedRecord).
+		WithDecrypter(entity.NewRsaDecrypter(keypair.PrivateKey)).
+		Build()
+
+	require.NoError(t, err)
+	assert.Equal(t, payload, string(decryptedRecord.Retrieve()))
+
+	hash, err := decryptedRecord.GetHash()
+	require.NoError(t, err)
+	assert.Equal(t, "96d59e2ea7cec4915c415431e6adb115e3c0c728928773bcc8e7d143b88bfda6", hash)
+}
+
+func testEcsdaSignature(t *testing.T, sdk client.Client) string {
+	keys, err := sdk.GenerateKeys()
+	require.NoError(t, err)
+
+	record, err := builder.
+		NewRecordBuilderFromString("Hello world 3").
+		WithSigner(entity.NewEcsdaSigner(keys.PrivateKey)).
+		Build()
+	require.NoError(t, err)
+
+	keys, err = sdk.GenerateKeys()
+	require.NoError(t, err)
+
+	recordWithMultipleSignatures, err := builder.
+		NewRecordBuilderFromRecord(record).
+		WithSigner(entity.NewEcsdaSigner(keys.PrivateKey)).
+		Build()
+
+	require.NoError(t, err)
+	hash, err := recordWithMultipleSignatures.GetHash()
+	require.NoError(t, err)
+	assert.Equal(t, hash, "79addac952bf2c80b87161407ac455cf389b17b98e8f3e75ed9638ab06481f4f")
+
+	signatures, err := recordWithMultipleSignatures.GetSignatures()
+	require.NoError(t, err)
+	assert.Equal(t, len(signatures), 2)
+
+	return hash
 }
--- languages/python/test/test_end_to_end.py
+++ languages/python/test/test_end_to_end.py
@@ -21,12 +21,113 @@ class TestE2E(unittest.TestCase):
         self.client = Client()
 
     def test_e2e_with_all_builders(self):
-        records = []
+        records = [
+            self._testFromString(),
+            self._testFromBytes(),
+            self._testFromHex(),
+            self._testFromJson(),
+            self._testFromFile(),
+            self._testEcsdaSignature(),
+        ]
+
+        self._testFromLoader()
+
+        self._testAesEncryption()
+        self._testAesEncryptionDataAvailability()
+
+        self._testRsaEncryption()
+        self._testRsaEncryptionDataAvailability()
+
+        send_receipts = self.client.send_records(records)
+        self.assertGreater(len(send_receipts), 0)
+
+        anchor = self.client.wait_anchor(send_receipts[0].anchor)
+        self.assertEqual(anchor.id, send_receipts[0].anchor)
+
+        proof = self.client.get_proof(records)
+
+        root = self.client.verify_proof(proof)
+
+        timestamp_validate_root = self.client.validate_root(root, Network.BLOOCK_CHAIN)
+        self.assertGreater(timestamp_validate_root, 0)
+
+        timestamp_verify_records = self.client.verify_records(
+            records, Network.BLOOCK_CHAIN
+        )
+        self.assertGreater(timestamp_verify_records, 0)
+
+        self.assertEqual(timestamp_verify_records, timestamp_validate_root)
+
+    def _testFromString(self) -> str:
         record = RecordBuilder.from_string("Hello world").build()
         hash = record.get_hash()
         self.assertEqual(
             hash, "ed6c11b0b5b808960df26f5bfc471d04c1995b0ffd2055925ad1be28d6baadfd"
         )
+        return hash
+
+    def _testFromBytes(self) -> str:
+        record = RecordBuilder.from_bytes(bytes([1, 2, 3, 4, 5])).build()
+        hash = record.get_hash()
+        self.assertEqual(
+            hash, "7d87c5ea75f7378bb701e404c50639161af3eff66293e9f375b5f17eb50476f4"
+        )
+        return hash
+
+    def _testFromHex(self) -> str:
+        record = RecordBuilder.from_hex("1234567890abcdef").build()
+        hash = record.get_hash()
+        self.assertEqual(
+            hash, "ed8ab4fde4c4e2749641d9d89de3d920f9845e086abd71e6921319f41f0e784f"
+        )
+        return hash
+
+    def _testFromJson(self) -> str:
+        record = RecordBuilder.from_json('{"hello":"world"}').build()
+        hash = record.get_hash()
+        self.assertEqual(
+            hash, "586e9b1e1681ba3ebad5ff5e6f673d3e3aa129fcdb76f92083dbc386cdde4312"
+        )
+        return hash
+
+    def _testFromFile(self) -> str:
+        record = RecordBuilder.from_file(bytes([2, 3, 4, 5, 6])).build()
+        hash = record.get_hash()
+        self.assertEqual(
+            hash, "507aa5dd7b2e52180b764db13c8289ed204109cafe2ef4e453366da8654dc446"
+        )
+        return hash
+
+    def _testEcsdaSignature(self) -> str:
+        keys = self.client.generate_keys()
+
+        record = (
+            RecordBuilder.from_string("Hello world 3")
+            .with_signer(EcsdaSigner(keys.private_key))
+            .build()
+        )
+
+        keys = self.client.generate_keys()
+
+        record_with_multiple_signatures = (
+            RecordBuilder.from_record(record)
+            .with_signer(EcsdaSigner(keys.private_key))
+            .build()
+        )
+
+        hash = record_with_multiple_signatures.get_hash()
+        self.assertEqual(
+            hash, "79addac952bf2c80b87161407ac455cf389b17b98e8f3e75ed9638ab06481f4f"
+        )
+
+        signatures = record_with_multiple_signatures.get_signatures()
+        self.assertEqual(len(signatures), 2)
+
+        return hash
+
+    def _testFromLoader(self) -> str:
+        record = RecordBuilder.from_string("Hello world").build()
+        hash = record.get_hash()
 
         result = record.publish(HostedPublisher())
         self.assertEqual(hash, result)
@@ -34,56 +135,68 @@ class TestE2E(unittest.TestCase):
         record = RecordBuilder.from_loader(HostedLoader(hash=result)).build()
         hash = record.get_hash()
         self.assertEqual(hash, result)
-        records.append(hash)
 
-        record = RecordBuilder.from_bytes(bytes([1, 2, 3, 4, 5])).build()
-        hash = record.get_hash()
-        self.assertEqual(
-            hash, "7d87c5ea75f7378bb701e404c50639161af3eff66293e9f375b5f17eb50476f4"
-        )
-        records.append(hash)
+        return hash
 
-        record = RecordBuilder.from_hex("1234567890abcdef").build()
-        hash = record.get_hash()
-        self.assertEqual(
-            hash, "ed8ab4fde4c4e2749641d9d89de3d920f9845e086abd71e6921319f41f0e784f"
-        )
-        records.append(hash)
-
-        record = RecordBuilder.from_json('{"hello":"world"}').build()
-        hash = record.get_hash()
-        self.assertEqual(
-            hash, "586e9b1e1681ba3ebad5ff5e6f673d3e3aa129fcdb76f92083dbc386cdde4312"
-        )
-        records.append(hash)
-
+    def _testAesEncryption(self):
         payload = "Hello world 2"
+        password = "some_password"
         encrypted_record = (
             RecordBuilder.from_string(payload)
-            .with_encrypter(AesEncrypter("some_password"))
+            .with_encrypter(AesEncrypter(password))
             .build()
         )
 
         self.assertNotEqual(payload.encode(), encrypted_record.payload)
-
-        record = (
-            RecordBuilder.from_record(encrypted_record)
-            .with_decrypter(AesDecrypter("some_password"))
-            .build()
-        )
-
-        self.assertEqual(payload, record.payload.decode())
-
-        hash = record.get_hash()
-        self.assertEqual(
-            "96d59e2ea7cec4915c415431e6adb115e3c0c728928773bcc8e7d143b88bfda6", hash
-        )
 
         with self.assertRaises(Exception) as _:
             RecordBuilder.from_record(encrypted_record).with_decrypter(
                 AesDecrypter("incorrect_password")
             ).build()
 
+        decrypted_record = (
+            RecordBuilder.from_record(encrypted_record)
+            .with_decrypter(AesDecrypter(password))
+            .build()
+        )
+
+        self.assertEqual(payload, decrypted_record.payload.decode())
+
+        hash = decrypted_record.get_hash()
+        self.assertEqual(
+            "96d59e2ea7cec4915c415431e6adb115e3c0c728928773bcc8e7d143b88bfda6", hash
+        )
+
+    def _testAesEncryptionDataAvailability(self):
+        payload = "Hello world 2"
+        password = "some_password"
+        encrypted_record = (
+            RecordBuilder.from_string(payload)
+            .with_encrypter(AesEncrypter(password))
+            .build()
+        )
+
+        self.assertNotEqual(payload.encode(), encrypted_record.payload)
+
+        result = encrypted_record.publish(HostedPublisher())
+
+        loaded_record = RecordBuilder.from_loader(HostedLoader(hash=result)).build()
+
+        decrypted_record = (
+            RecordBuilder.from_record(loaded_record)
+            .with_decrypter(AesDecrypter(password))
+            .build()
+        )
+
+        self.assertEqual(payload, decrypted_record.payload.decode())
+
+        hash = decrypted_record.get_hash()
+        self.assertEqual(
+            "96d59e2ea7cec4915c415431e6adb115e3c0c728928773bcc8e7d143b88bfda6", hash
+        )
+
+    def _testRsaEncryption(self):
+        payload = "Hello world 2"
         keys = self.client.generate_rsa_keypair()
 
         encrypted_record = (
@@ -107,59 +220,34 @@ class TestE2E(unittest.TestCase):
             "96d59e2ea7cec4915c415431e6adb115e3c0c728928773bcc8e7d143b88bfda6", hash
         )
 
-        records.append(hash)
+    def _testRsaEncryptionDataAvailability(self):
+        payload = "Hello world 2"
+        keys = self.client.generate_rsa_keypair()
 
-        record = RecordBuilder.from_file(bytes([2, 3, 4, 5, 6])).build()
-        hash = record.get_hash()
-        self.assertEqual(
-            hash, "507aa5dd7b2e52180b764db13c8289ed204109cafe2ef4e453366da8654dc446"
+        encrypted_record = (
+            RecordBuilder.from_string(payload)
+            .with_encrypter(RsaEncrypter(keys.public_key))
+            .build()
         )
-        records.append(hash)
 
-        keys = self.client.generate_keys()
+        self.assertNotEqual(payload.encode(), encrypted_record.payload)
+
+        result = encrypted_record.publish(HostedPublisher())
+
+        loaded_record = RecordBuilder.from_loader(HostedLoader(hash=result)).build()
 
         record = (
-            RecordBuilder.from_string("Hello world 3")
-            .with_signer(EcsdaSigner(keys.private_key))
+            RecordBuilder.from_record(loaded_record)
+            .with_decrypter(RsaDecrypter(keys.private_key))
             .build()
         )
 
-        keys = self.client.generate_keys()
+        self.assertEqual(payload, record.payload.decode())
 
-        record_with_multiple_signatures = (
-            RecordBuilder.from_record(record)
-            .with_signer(EcsdaSigner(keys.private_key))
-            .build()
-        )
-
-        hash = record_with_multiple_signatures.get_hash()
+        hash = record.get_hash()
         self.assertEqual(
-            hash, "79addac952bf2c80b87161407ac455cf389b17b98e8f3e75ed9638ab06481f4f"
+            "96d59e2ea7cec4915c415431e6adb115e3c0c728928773bcc8e7d143b88bfda6", hash
         )
-        records.append(hash)
-
-        signatures = record_with_multiple_signatures.get_signatures()
-        self.assertEqual(len(signatures), 2)
-
-        send_receipts = self.client.send_records(records)
-        self.assertGreater(len(send_receipts), 0)
-
-        anchor = self.client.wait_anchor(send_receipts[0].anchor)
-        self.assertEqual(anchor.id, send_receipts[0].anchor)
-
-        proof = self.client.get_proof(records)
-
-        root = self.client.verify_proof(proof)
-
-        timestamp_validate_root = self.client.validate_root(root, Network.BLOOCK_CHAIN)
-        self.assertGreater(timestamp_validate_root, 0)
-
-        timestamp_verify_records = self.client.verify_records(
-            records, Network.BLOOCK_CHAIN
-        )
-        self.assertGreater(timestamp_verify_records, 0)
-
-        self.assertEqual(timestamp_verify_records, timestamp_validate_root)
 
 
 if __name__ == "__main__":
