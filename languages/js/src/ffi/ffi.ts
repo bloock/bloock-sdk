0564f6f379f55bc827a0dc55fbf22fcd5c6e8709
josepmdc <jdomingocatafal@gmail.com>
2022-12-22 11:17:39 +0100

    Make FFIClient a singleton to fix WASM memory out of bounds error when creating multiple instances

--- languages/js/src/ffi/ffi.ts
+++ languages/js/src/ffi/ffi.ts
@@ -2,15 +2,24 @@ import { base64ToBytes, bytesToBase64 } from "./base64";
 import init, { request } from "./native/bloock_bridge";
 
 export class FFIClient {
+  private static instance: FFIClient;
   private ready: Promise<void>;
 
-  constructor() {
+  private constructor() {
     this.ready = new Promise((resolve, reject) => {
       this.init()
         .then(resolve)
         .catch(reject);
     });
   }
+
+  static getInstance() {
+      if (!FFIClient.instance) {
+          FFIClient.instance = new FFIClient();
+      }
+      return FFIClient.instance;
+  }
+
   public async request(type: string, payload: Uint8Array): Promise<Uint8Array> {
     await this.ready;
 
