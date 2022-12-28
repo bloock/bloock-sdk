2ac42c1289bcff9891bd50a4d02216686800fc3e
josepmdc <jdomingocatafal@gmail.com>
2022-12-28 12:34:50 +0100

    Implement tests for IPFS on core service

--- bloock-core/src/publish/service.rs
+++ bloock-core/src/publish/service.rs
@@ -83,6 +83,7 @@ mod tests {
     use mockall::predicate::eq;
     use std::sync::Arc;
 
+    use crate::publish::entity::publish_ipfs_response::PublishIpfsResponse;
     use crate::publish::{self, entity::publish_hosted_response::PublishHostedResponse};
     use crate::record::document::Document;
     use crate::record::entity::record::Record;
@@ -131,6 +132,62 @@ mod tests {
         let service = publish::configure_test(Arc::new(http));
         let result = service
             .retrieve_hosted(
+                "c5a2180e2f97506550f1bba5d863bc6ed05ad8b51daf6fca1ac7d396ef3183c5".to_owned(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(
+            payload,
+            result.as_slice(),
+            "Retrieved payload should match expected"
+        )
+    }
+
+    #[tokio::test]
+    async fn ipfs_publish_file() {
+        let payload: Vec<u8> = vec![1, 2, 3, 4, 5];
+        let response = PublishIpfsResponse {
+            hash: "c5a2180e2f97506550f1bba5d863bc6ed05ad8b51daf6fca1ac7d396ef3183c5".to_owned(),
+        };
+
+        let mut http = MockClient::default();
+        http.expect_post_file::<String, PublishIpfsResponse>()
+            .with(
+                eq("https://api.bloock.com/hosting/v1/ipfs/upload".to_owned()),
+                eq(vec![("payload".to_owned(), payload.to_vec())]),
+                eq(None),
+            )
+            .return_once(|_, _, _| Ok(response));
+
+        let service = publish::configure_test(Arc::new(http));
+
+        let record = Record::new(Document::new(&payload).unwrap());
+
+        let result = service.publish_ipfs(record).await.unwrap();
+
+        assert_eq!(
+            &result, "c5a2180e2f97506550f1bba5d863bc6ed05ad8b51daf6fca1ac7d396ef3183c5",
+            "Should not return an empty result"
+        )
+    }
+
+    #[tokio::test]
+    async fn ipfs_retrieve_file() {
+        let payload = [1, 2, 3, 4, 5];
+        let hash = "c5a2180e2f97506550f1bba5d863bc6ed05ad8b51daf6fca1ac7d396ef3183c5";
+
+        let mut http = MockClient::default();
+        http.expect_get::<String>()
+            .with(
+                eq(format!("https://api.bloock.com/hosting/v1/ipfs/{}", hash)),
+                eq(None),
+            )
+            .return_once(move |_, _| Ok(payload.to_vec()));
+
+        let service = publish::configure_test(Arc::new(http));
+        let result = service
+            .retrieve_ipfs(
                 "c5a2180e2f97506550f1bba5d863bc6ed05ad8b51daf6fca1ac7d396ef3183c5".to_owned(),
             )
             .await
