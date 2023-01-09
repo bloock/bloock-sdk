d22d8832c9b675fbbd1a6cf8d6d9f3bec42fc08d
josepmdc <jdomingocatafal@gmail.com>
2023-01-09 15:55:21 +0100

    Implement verify webhook signature Java interface

--- languages/java/src/main/java/com/bloock/sdk/bridge/Bridge.java
+++ languages/java/src/main/java/com/bloock/sdk/bridge/Bridge.java
@@ -3,17 +3,20 @@ package com.bloock.sdk.bridge;
 import com.bloock.sdk.bridge.proto.AnchorServiceGrpc;
 import com.bloock.sdk.bridge.proto.ProofServiceGrpc;
 import com.bloock.sdk.bridge.proto.RecordServiceGrpc;
+import com.bloock.sdk.bridge.proto.WebhookServiceGrpc;
 
 public class Bridge {
   private final AnchorServiceGrpc.AnchorServiceBlockingStub anchor;
   private final RecordServiceGrpc.RecordServiceBlockingStub record;
   private final ProofServiceGrpc.ProofServiceBlockingStub proof;
+  private final WebhookServiceGrpc.WebhookServiceBlockingStub webhook;
 
   public Bridge() {
     Connection conn = new Connection();
     anchor = AnchorServiceGrpc.newBlockingStub(conn);
     record = RecordServiceGrpc.newBlockingStub(conn);
     proof = ProofServiceGrpc.newBlockingStub(conn);
+    webhook = WebhookServiceGrpc.newBlockingStub(conn);
   }
 
   public AnchorServiceGrpc.AnchorServiceBlockingStub getAnchor() {
@@ -26,5 +29,9 @@ public class Bridge {
 
   public ProofServiceGrpc.ProofServiceBlockingStub getProof() {
     return this.proof;
+  }
+
+  public WebhookServiceGrpc.WebhookServiceBlockingStub getWebhook() {
+    return this.webhook;
   }
 }
--- languages/java/test/Test.java
+++ languages/java/test/Test.java
@@ -41,6 +41,14 @@ class Test {
 
   public static void main(String[] args) throws Exception {
     Client sdk = getSdk();
+
+    testVerifyWebhookSignatureOk(sdk);
+    testVerifyWebhookSignatureInvalidSecret(sdk);
+
+    testEnd2End(sdk);
+  }
+
+  static void testEnd2End(Client sdk) throws Exception {
     ArrayList<Record> records = new ArrayList<>();
 
     records.add(testFromString());
@@ -404,5 +412,27 @@ class Test {
     assert originalProof.getAnchor().getAnchorId() == finalProof.getAnchor().getAnchorId();
     assert originalProof.getAnchor().getRoot().equals(finalProof.getAnchor().getRoot());
     assert originalProof.getAnchor().getStatus().equals(finalProof.getAnchor().getStatus());
+  }
+
+  private static void testVerifyWebhookSignatureOk(Client client) throws Exception {
+    String payload =
+        "{\"webhook_id\":\"80b505b4-df81-48f4-92a6-69cbc1a114a0\",\"request_id\":\"8d30ef45-69a3-4cdd-8457-8cad485848f2\",\"type\":\"core.bloock_chain\",\"created_at\":1672909660,\"data\":{\"created_at\":1672909591,\"finalized\":true,\"id\":105122,\"message_count\":1,\"network\":{\"anchor_id\":105122,\"created_at\":1672909661,\"name\":\"bloock_chain\",\"status\":\"Confirmed\",\"test\":false,\"tx_hash\":\"0x53d1c7c1ff8100b921ce0ef593c81ed4e5e50eff888b3bb5c69260c13f0b2f58\"},\"root\":\"fd71e7ac128ff2219853e43e4044769df36c5329ce34655b4c5d166d1564d5b7\",\"test\":false}}";
+    String header =
+        "t=1672909660,v1=955e726c98d606ff5534d325f68854173411be61698ef7c5c466a5485f979a29";
+    String secret = "NHJTAE6ikKBccSaeCSBSWGdp7NmixXy7";
+
+    boolean isValid = client.verifyWebhookSignature(payload.getBytes(), header, secret, false);
+    assert isValid;
+  }
+
+  private static void testVerifyWebhookSignatureInvalidSecret(Client client) throws Exception {
+    String payload =
+        "{\"webhook_id\":\"80b505b4-df81-48f4-92a6-69cbc1a114a0\",\"request_id\":\"8d30ef45-69a3-4cdd-8457-8cad485848f2\",\"type\":\"core.bloock_chain\",\"created_at\":1672909660,\"data\":{\"created_at\":1672909591,\"finalized\":true,\"id\":105122,\"message_count\":1,\"network\":{\"anchor_id\":105122,\"created_at\":1672909661,\"name\":\"bloock_chain\",\"status\":\"Confirmed\",\"test\":false,\"tx_hash\":\"0x53d1c7c1ff8100b921ce0ef593c81ed4e5e50eff888b3bb5c69260c13f0b2f58\"},\"root\":\"fd71e7ac128ff2219853e43e4044769df36c5329ce34655b4c5d166d1564d5b7\",\"test\":false}}";
+    String header =
+        "t=1672909660,v1=955e726c98d606ff5534d325f68854173411be61698ef7c5c466a5485f979a29";
+    String secret = "asdf";
+
+    boolean isValid = client.verifyWebhookSignature(payload.getBytes(), header, secret, false);
+    assert !isValid;
   }
 }
