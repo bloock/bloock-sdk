dd433dbec8c4762ea2ac783e5dc406fa20a0a6e7
marcbaque <marc@bloock.com>
2023-02-21 15:38:32 +0100

    Fix php binaries location

--- languages/php/Bloock/Ffi/Ffi.php
+++ languages/php/Bloock/Ffi/Ffi.php
@@ -27,22 +27,20 @@ final class Ffi
 
     private function getBinaryPath(): string
     {
+        $libDirs = dirname(__FILE__);
         $arch = php_uname("m");
         if (PHP_OS == 'Darwin') {
             if ($arch == 'arm64') {
-                return dirname(__FILE__) . '/native/aarch64_apple_darwin/libbloock_bridge.dylib';
+                $libDirs = $libDirs . '/native/aarch64_apple_darwin/libbloock_bridge.dylib';
             } else {
-                return dirname(__FILE__) . '/native/x86_64_apple_darwin/libbloock_bridge.dylib';
+                $libDirs = $libDirs . '/native/x86_64_apple_darwin/libbloock_bridge.dylib';
             }
         } else if (PHP_OS == 'Linux') {
-            exec('getconf GNU_LIBC_VERSION', $output, $retVar);
-            if ($retVar === 0) {
-                return dirname(__FILE__) . '/native/linux/gnu.so';
-            }
-
-            return dirname(__FILE__) . '/native/x86_64_unknown_linux_musl/libbloock_bridge.dylib';
-        } else {
-            return dirname(__FILE__) . '/native/x86_64_unknown_linux_musl/libbloock_bridge.dylib';
+            $libDirs = $libDirs . '/native/x86_64_unknown_linux_musl/libbloock_bridge.so';
+        } else if (PHP_OS == 'Windows') {
+            $libDirs = $libDirs . '/native/x86_64_pc_windows_gnu/libbloock_bridge.dll';
         }
+
+        return $libDirs;
     }
 }
