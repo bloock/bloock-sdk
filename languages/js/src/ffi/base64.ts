64cdcf4b49e4784ca3629f1daf12e1debd0329ae
Marc Baqué <marcbaque@Marcs-MacBook-Pro.local>
2025-09-16 19:55:09 +0200

    js: changed base64 encode/decode library

--- languages/js/src/ffi/base64.ts
+++ languages/js/src/ffi/base64.ts
@@ -1,7 +1,9 @@
+import { Base64 } from 'js-base64';
+
 export const base64ToBytes = (base64: string): Uint8Array => {
-  return new Uint8Array(Buffer.from(base64, "base64"));
+  return Base64.toUint8Array(base64);
 };
 
 export const bytesToBase64 = (bytes: Uint8Array): string => {
-  return Buffer.from(bytes).toString("base64")
+  return Base64.fromUint8Array(bytes)
 };
